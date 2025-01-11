1b33711bc24a8c5a93c1f52c2d926ad6f45a2ae3
Ray Lopez <raylopez509@gmail.com>
2025-01-11 13:18:27 -0800

    added playable tic tac toe in console doesn't have unselectable squares, but I probably don't need to change that when changing logic to the DOM

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-const players = [
-  {
-    name: 'playerOneName',
-    marker: 'X',
-  },
-  {
-    name: 'playerTwoName',
-    marker: 'O',
-  },
-];
-
 function GameBoard() {
   const gameBoard = ['', '', '', '', '', '', '', '', ''];
   function displayBoard() {
@@ -25,8 +14,8 @@ function GameBoard() {
       }
     }
   }
-  function updateBoard(square, player) {
-    gameBoard[square] = player.marker;
+  function updateBoard(square, marker) {
+    gameBoard[square] = marker;
   }
   return {
     displayBoard,
@@ -34,8 +23,87 @@ function GameBoard() {
   };
 }
 
-const board = GameBoard();
-board.displayBoard();
+function GameController() {
+  const players = [
+    {
+      name: 'playerOneName',
+      marker: 'X',
+      choices: [],
+    },
+    {
+      name: 'playerTwoName',
+      marker: 'O',
+      choices: [],
+    },
+  ];
 
-board.updateBoard(0, players[0]);
-board.displayBoard();
+  const board = GameBoard();
+
+  let activePlayer = players[0];
+
+  const swapPlayers = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+
+  const playTurn = (player) => {
+    board.displayBoard();
+    let choice = prompt('Choose a square ' + player.name);
+    console.log(choice);
+    player.choices.push(choice);
+    board.updateBoard(choice, player.marker);
+  };
+
+  function checkIfWon() {
+    if (
+      (activePlayer.choices.includes('0') &&
+        activePlayer.choices.includes('1') &&
+        activePlayer.choices.includes('2')) ||
+      (activePlayer.choices.includes('3') &&
+        activePlayer.choices.includes('4') &&
+        activePlayer.choices.includes('5')) ||
+      (activePlayer.choices.includes('6') &&
+        activePlayer.choices.includes('7') &&
+        activePlayer.choices.includes('8')) ||
+      (activePlayer.choices.includes('0') &&
+        activePlayer.choices.includes('3') &&
+        activePlayer.choices.includes('6')) ||
+      (activePlayer.choices.includes('1') &&
+        activePlayer.choices.includes('4') &&
+        activePlayer.choices.includes('7')) ||
+      (activePlayer.choices.includes('2') &&
+        activePlayer.choices.includes('5') &&
+        activePlayer.choices.includes('8')) ||
+      (activePlayer.choices.includes('0') &&
+        activePlayer.choices.includes('4') &&
+        activePlayer.choices.includes('8')) ||
+      (activePlayer.choices.includes('2') &&
+        activePlayer.choices.includes('4') &&
+        activePlayer.choices.includes('6'))
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  const playGame = () => {
+    let wonGame = false;
+    while (!wonGame) {
+      playTurn(activePlayer);
+      wonGame = checkIfWon();
+      if (!wonGame) {
+        swapPlayers();
+      }
+    }
+    console.log(activePlayer.name + ' has won!');
+  };
+
+  return {
+    playTurn,
+    playGame,
+  };
+}
+
+let game = GameController();
+
+game.playGame();
